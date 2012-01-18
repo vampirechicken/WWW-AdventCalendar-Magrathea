0e6e17bf81e423251351bcaf6977f0082769021c
Len Jaffe <lenjaffe@jaffesystems.com>
2012-01-18 13:28:36 -0500

    - refactored preprocesspod.pl code into initialize_year() - refactored mkadvent.sh into a loop over years - removed generated files.

--- mkadvent.sh
+++ mkadvent.sh
@@ -6,27 +6,27 @@ ADVCAL="/usr/bin/env advcal"
 PERL="/usr/bin/env perl"
 SCP="/usr/bin/env scp"
 
-YEAR=$1
-OUTDIR=out/${YEAR}
-CONFIGDIR=config/${YEAR}
-ARTICLE_DIR=articles/post/${YEAR}
-URI_BASE=www.theycomewithcheese.com/PerlAdventPlanet/${YEAR}/
-#URI=http://${URI_BASE}
+while [ -n "$1" ]; do
+  YEAR=$1
+  OUTDIR=out/${YEAR}
+  CONFIGDIR=config/${YEAR}
+  ARTICLE_DIR=articles/post/${YEAR}
+  URI_BASE=theycomewithcheese.com/PerlAdventPlanet/${YEAR}
+  #URI=http://${URI_BASE}
 
-HTML_ROOT=laj@vampirechicken.com:~/${URI_BASE}
+  HTML_ROOT=laj@vampirechicken.com:/home/laj/${URI_BASE}
 
-cd ${RUNDIR}
-if [ ! -d $OUTDIR ]; then
-  mkdir -p $OUTDIR
-fi
+  cd ${RUNDIR}
+  if [ ! -d $OUTDIR ]; then
+    mkdir -p $OUTDIR
+  fi
 
-if [ ! -d $CONFIGDIR ]; then
-  mkdir -p $CONFIGDIR
-fi
+  if [ ! -d $CONFIGDIR ]; then
+    mkdir -p $CONFIGDIR
+  fi
 
-${PERL} preprocesspod.pl -v $YEAR
-${ADVCAL} -c ${CONFIGDIR}/advent.ini --article-dir ${ARTICLE_DIR} --out ${OUTDIR}
-${SCP} out/${YEAR}/* ${HTML_ROOT}
-
-
-
+  ${PERL} preprocesspod.pl -v $YEAR
+  ${ADVCAL} -c ${CONFIGDIR}/advent.ini --article-dir ${ARTICLE_DIR} --out ${OUTDIR}
+  ${SCP} out/${YEAR}/* ${HTML_ROOT}
+  shift
+done
--- preprocesspod.pl
+++ preprocesspod.pl
@@ -8,8 +8,8 @@ use feature ':5.14';
 use HTTP::Tiny;
 use HTML::Template;
 
+use File::Path qw(make_path);
 use IO::Any;
-
 
 my $verbose = 0;  # status messages to STDERR if $verbose == 1
 if ($ARGV[0] && $ARGV[0] eq '-v') {
@@ -22,62 +22,66 @@ my $usage = "$0 [-v] year";
 my $year = shift @ARGV;
 die $usage unless $year;
 
-my $template_dir      = "articles/templates/$year";
-unless (-e $template_dir && -d _) {
-   die "$template_dir is not a directory";
-}
-
-my $article_tmpl_name = 'article.pod.tmpl';
-my $article_tmpl_file = "$template_dir/$article_tmpl_name";
-my $article_tmpl      = initialize_template($article_tmpl_file, $year);
-
-my $pre_dir  = "articles/pre/$year";  # this is where the processed templates get written. It gets preprocessed into the @post_dir
-my $post_dir = "articles/post/$year"; # these files are the souce for advcal
-
-
-my $config_tmpl_file = "$template_dir/advent.ini.tmpl";
-my $config_dir       = "config/$year";
-
-foreach my $dir ( $pre_dir, $post_dir, $config_dir )  {
-  unless (-e $dir && -d _) {
-     die "$dir is not a directory";
-  }
-}
-
-my $config_file      = "$config_dir/advent.ini";
-make_config_file($config_tmpl_file, $config_file, $year);
-
-my $web = HTTP::Tiny->new;
+my $config = initialize_year($year);
+my $web    = HTTP::Tiny->new;
 foreach my $day ( 1..24 ) {
-  $article_tmpl->param( DAY => $day );
+  $config->{article_tmpl}->param( DAY => $day );
 
   my $day02 = sprintf "%02d", $day;
-  $article_tmpl->param( DAY02 => $day02 );
+  $config->{article_tmpl}->param( DAY02 => $day02 );
 
-  my $podfile  = "$year-12-$day02.pod" ;
+  my $podname  = "$year-12-$day02.pod" ;
+  my $prefile  = "$config->{pre_dir}/$podname";
+  my $postfile = "$config->{post_dir}/$podname" ;
 
-  my $prefile  = "$pre_dir/$podfile";
-  make_prefile($prefile, $verbose);
-
-  my $postfile = "$post_dir/$podfile" ;
+  make_prefile($prefile, $config, $verbose);
   preprocess($prefile, $postfile, $verbose);
 }
 
+sub initialize_year {
+  my $year = shift;
+  my $config;
+
+  $config->{pre_dir}  = "articles/pre/$year";  # this is where the processed templates get written. It gets preprocessed into the @post_dir
+  $config->{post_dir} = "articles/post/$year"; # these files are the souce for advcal
+  my $config_dir      = "config/$year";
+  my $template_dir    = "articles/templates/$year";
+  foreach my $dir ( $template_dir, $config->{pre_dir}, $config->{post_dir}, $config_dir )  {
+    make_dir($dir);
+  }
+
+  my $article_tmpl_name   = 'article.pod.tmpl';
+  my $article_tmpl_file   = "$template_dir/$article_tmpl_name";
+  $config->{article_tmpl} = initialize_template($article_tmpl_file, $year);
+
+  my $config_tmpl_file = "$template_dir/advent.ini.tmpl";
+  my $config_file      = "$config_dir/advent.ini";
+  make_config_file($config_tmpl_file, $config_file, $year);
+
+  return $config;
+}
+
+sub make_dir {
+  my $dir = shift;
+  return if -e -d $dir;
+  make_path($dir, { mode => 0644 }) || die "could not create $dir: $!";
+}
 
 sub make_prefile {
   my $prefile = shift;
+  my $config  = shift;
   my $verbose = shift;
   my $prefh = IO::Any->write($prefile) || die "Could not open the preprocess file for writing: $!";
 
   say STDERR "Creating $prefile" if $verbose;
-  print $prefh $article_tmpl->output;
+  print $prefh $config->{article_tmpl}->output;
   close $prefh;
 }
 
 
 sub make_config_file {
   my ($tmpl_file, $config_file, $year) = @_;
-  my $config_template = initialize_template($tmpl_file, $year); 
+  my $config_template = initialize_template($tmpl_file, $year);
 
   my $config_fh = IO::Any->write($config_file) or die "Could not write $config_file";
   print $config_fh $config_template->output;
@@ -86,7 +90,7 @@ sub make_config_file {
 
 sub initialize_template {
   my ($tmpl_file, $year) = @_;
-  
+
   my $template = HTML::Template->new(filename => $tmpl_file);
   $template->param( YEAR => $year );
   return $template;
@@ -94,7 +98,7 @@ sub initialize_template {
 
 
 
-sub preprocess {  
+sub preprocess {
   my ($prefile, $postfile, $verbose) = @_;
 
   my $prefh = IO::Any->read($prefile);
@@ -117,15 +121,15 @@ sub preprocess {
     }
     my ($label, $url) = split(/\|/, $+{link});
     say STDERR sprintf("\tprocessing %32s: %s", $label, $url) if $verbose;
-  
+
     my $response = $web->get($url);
     if ( $response->{status} != 200) {
-       say $postfh "${label}: unavailable";
+       say $postfh "${label}: is unavailable.";
        next;
     }
-  
+
     if ( $response->{content} =~ m!<title>(?<title>.*)</title>!s ) {
-        $label .= ": $+{title}"; 
+        $label .= ": $+{title}";
         $label =~ s/[|]/-/mg;
         $label =~ s/\n/ /mg;
     }
@@ -136,6 +140,4 @@ sub preprocess {
   close $prefh;
   close $postfh;
 }
-
-
 
