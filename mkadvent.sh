40a30124252695e2765eff748446fc0defbda5d1
Len Jaffe <lenjaffe@jaffesystems.com>
2014-08-10 05:24:31 -0400

    update perlbrew perl to 5.18.2

--- mkadvent.sh
+++ mkadvent.sh
@@ -6,7 +6,7 @@ echo "$0 Started: " `date`
 export PERLBREW_ROOT=/home/len/perl5/perlbrew
 export PERLBREW_HOME=/home/len/.perlbrew
 . ${PERLBREW_ROOT}/etc/bashrc
-perlbrew use 5.18.0
+perlbrew use 5.18.2
 
 RUNDIR=/home/len/AdventPlanet/WWW-AdventCalendar-Magrathea
 
